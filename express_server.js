a2fa87c4a632d05d827bce29b5bdc7492bd136eb
Rheaazarraga <rheaazarraga@gmail.com>
2021-08-30 23:31:45 +0000

    Creating a form that allows a user to input a longURL and sends the data to our API via POST request. Also adding a route which renders this form when the user visits urls/new. Utilize bodu-parser library to make the POST request body human readable, then generate a random string to serve as the shortURL

--- express_server.js
+++ express_server.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+
+const generateRandomString = function () {
+  return (Math.random() + 1).toString(36).substr(6);
+};
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({extended: true})); //added this & line 13 because bodyParser kept deprecating
+app.use(express.json())
+
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": "http://www.lighthouselabs.ca", //shortURL : longURL
+  "9sm5xK": "http://www.google.com",
 };
+
+//GET ROUTE HANDLERS
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -22,8 +33,15 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls/:shortURL", (req, res) => { // ':' indicates the ID is a route parameter
-  const templateVars = { shortURL: req.params.shortURL, longURL: 'https://www.lighthouselabs.ca/'};
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  // ':' indicates the ID is a route parameter
+  let shortURL = req.params.shortURL;
+  let longURL = urlDatabase[shortURL];
+  const templateVars = { longURL: longURL, shortURL: shortURL };
   res.render("urls_show", templateVars);
 });
 
@@ -31,6 +49,12 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+//PORT LISTENER
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
